233b728a602908d91d60bd44e5f36a0102f2df58
Mohammad Babayi <chief.mb1976@gmail.com>
2023-05-09 12:07:57 +0330

    ~ AddFromServiceDescriptionProviders > AddServiceSetups

--- src/GihanSoft.Framework/Bootstrap/Services/ServiceProviderServicesExtensions.cs
+++ src/GihanSoft.Framework/Bootstrap/Services/ServiceProviderServicesExtensions.cs
@@ -24,38 +24,38 @@ public static class ServiceProviderServicesExtensions
             });
     }
 
-    public static IServiceCollection AddFromServiceDescriptionProviders(this IServiceCollection services, IServiceProvider serviceProvider, params Assembly[] assemblies)
+    public static IServiceCollection AddServiceSetups(this IServiceCollection services, IServiceProvider serviceProvider, params Assembly[] assemblies)
     {
         return AddServiceSetups(services, serviceProvider, assemblies.AsEnumerable());
     }
 
-    public static IServiceCollection AddFromServiceDescriptionProviders(this IServiceCollection services, IServiceProvider serviceProvider, IEnumerable<Type> types)
+    public static IServiceCollection AddServiceSetups(this IServiceCollection services, IServiceProvider serviceProvider, IEnumerable<Type> types)
     {
         return AddServiceSetups(services, serviceProvider, types.Select(t => t.Assembly));
     }
 
-    public static IServiceCollection AddFromServiceDescriptionProviders(this IServiceCollection services, IServiceProvider serviceProvider, params Type[] types)
+    public static IServiceCollection AddServiceSetups(this IServiceCollection services, IServiceProvider serviceProvider, params Type[] types)
     {
         return AddServiceSetups(services, serviceProvider, types.Select(t => t.Assembly));
     }
 
-    public static IServiceCollection AddFromServiceDescriptionProviders<TMarker>(this IServiceCollection services, IServiceProvider serviceProvider)
+    public static IServiceCollection AddServiceSetups<TMarker>(this IServiceCollection services, IServiceProvider serviceProvider)
     {
-        return AddFromServiceDescriptionProviders(services, serviceProvider, typeof(TMarker).Assembly);
+        return AddServiceSetups(services, serviceProvider, typeof(TMarker).Assembly);
     }
 
-    public static IServiceCollection AddFromServiceDescriptionProviders<TMarker1, TMarker2>(this IServiceCollection services, IServiceProvider serviceProvider)
+    public static IServiceCollection AddServiceSetups<TMarker1, TMarker2>(this IServiceCollection services, IServiceProvider serviceProvider)
     {
-        return AddFromServiceDescriptionProviders(services, serviceProvider, typeof(TMarker1).Assembly, typeof(TMarker2).Assembly);
+        return AddServiceSetups(services, serviceProvider, typeof(TMarker1).Assembly, typeof(TMarker2).Assembly);
     }
 
-    public static IServiceCollection AddFromServiceDescriptionProviders<TMarker1, TMarker2, TMarker3>(this IServiceCollection services, IServiceProvider serviceProvider)
+    public static IServiceCollection AddServiceSetups<TMarker1, TMarker2, TMarker3>(this IServiceCollection services, IServiceProvider serviceProvider)
     {
-        return AddFromServiceDescriptionProviders(services, serviceProvider, typeof(TMarker1).Assembly, typeof(TMarker2).Assembly, typeof(TMarker3).Assembly);
+        return AddServiceSetups(services, serviceProvider, typeof(TMarker1).Assembly, typeof(TMarker2).Assembly, typeof(TMarker3).Assembly);
     }
 
-    public static IServiceCollection AddFromServiceDescriptionProviders<TMarker1, TMarker2, TMarker3, TMarker4>(this IServiceCollection services, IServiceProvider serviceProvider)
+    public static IServiceCollection AddServiceSetups<TMarker1, TMarker2, TMarker3, TMarker4>(this IServiceCollection services, IServiceProvider serviceProvider)
     {
-        return AddFromServiceDescriptionProviders(services, serviceProvider, typeof(TMarker1).Assembly, typeof(TMarker2).Assembly, typeof(TMarker3).Assembly, typeof(TMarker4).Assembly);
+        return AddServiceSetups(services, serviceProvider, typeof(TMarker1).Assembly, typeof(TMarker2).Assembly, typeof(TMarker3).Assembly, typeof(TMarker4).Assembly);
     }
 }
